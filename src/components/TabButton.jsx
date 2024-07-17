813bcbe52c787aa5524e5f75608433c871b2b7f9
Vladimir <vltinica@gmail.com>
2024-07-17 12:12:54 -0400

    some change for button

--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 
 const TabButton = ({ children }) => {
+  function handlerClick() {
+    console.log("Hello Vladimir");
+  }
+
   return (
     <div>
       <li>
-        <button>{children}</button>
+        <button onClick={handlerClick}>{children}</button>
       </li>
     </div>
   );
